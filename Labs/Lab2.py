8ccdb35d6a8714b24cdbfd551e600daf9e323763
Monica-Lillie <152418293+mojacobs09@users.noreply.github.com>
2026-01-29 19:42:35 +0000

    Updates

---
+++ Labs/Lab2.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from openai import OpenAI
+
+# Show title and description.
+st.title("My Document question answering")
+st.write(
+    "Upload a document below and ask a question about it – GPT will answer! "
+    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+)
+
+openai_api_key = st.secrets.OPENAI_API_KEY
+
+# Create an OpenAI client.
+client = OpenAI(api_key=openai_api_key)
+
+# Let the user upload a file via `st.file_uploader`.
+uploaded_file = st.file_uploader(
+     "Upload a document (.txt or .md)", type=("txt", "md")
+
+# Ask the user for a question via `st.text_area`.
+question = st.text_area(
+    "Now ask a question about the document!",
+    placeholder="Can you give me a short summary?",
+    disabled=not uploaded_file,
+)
+if uploaded_file and question:
+    # Process the uploaded file and question.
+    document = uploaded_file.read().decode()
+    messages = [
+        {
+            "role": "user",
+            "content": f"Here's a document: {document} \n\n---\n\n {question}",
+        }
+    ]
+    # Generate an answer using the OpenAI API.
+    stream = client.chat.completions.create(
+        model="gpt-5-chat-latest",
+        messages=messages,
+        stream=True,
+    )
+    # Stream the response to the app using `st.write_stream`.
+    st.write_stream(stream)
