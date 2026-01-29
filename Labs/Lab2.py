4dd74b177b0a1956fc00c899eedd7738a1eaa2dd
Monica-Lillie <152418293+mojacobs09@users.noreply.github.com>
2026-01-29 21:41:54 +0000

    Updated Part C

--- Labs/Lab2.py
+++ Labs/Lab2.py
@@ -18,11 +18,7 @@ uploaded_file = st.file_uploader(
      "Upload a document (.txt or .md)", type=("txt", "md")
 
 # Ask the user for a question via `st.text_area`.
-question = st.text_area(
-    "Now ask a question about the document!",
-    placeholder="Can you give me a short summary?",
-    disabled=not uploaded_file,
-)
+
 if uploaded_file and question:
     # Process the uploaded file and question.
     document = uploaded_file.read().decode()
@@ -32,6 +28,24 @@ if uploaded_file and question:
             "content": f"Here's a document: {document} \n\n---\n\n {question}",
         }
     ]
+
+    #summary choice
+
+    st.sidebar.header(":red[Summary Type]") 
+    summary_type = st.sidebar.selectbox (
+        "Choose a summary type",
+        ('Summarize in 100 words', 'Summarize in 2 connecting paragraghs', 'Summarize in 5 bullet points')
+
+    )
+# check box make the user select the higher model
+    use_advance = st.checkbox('Use Advanced Model')
+
+    if use_advance:
+        model_name = 'gpt-5-mini'
+    else: 
+        model_name = "gpt-5.1"
+
+
     # Generate an answer using the OpenAI API.
     stream = client.chat.completions.create(
         model="gpt-5-chat-latest",
