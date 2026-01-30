578bf0ac07b842d22ee002f802452c6fb9c6b920
Monica-Lillie <152418293+mojacobs09@users.noreply.github.com>
2026-01-30 02:34:21 +0000

    updated code

--- streamlit_app-3.py
+++ streamlit_app-3.py
@@ -1,10 +1,11 @@
 import streamlit as st
 
-st.title ("IST 488 Labs")
-lab1 = st.Page('Labs/Lab1.py', title = 'Lab 1')
-lab2 = st.Page('Labs/Lab2.py', title = 'Lab 2', default = True)
-pg = st.navigation([lab2, lab1])
 
 st.set_page_config(page_title = 'IST 488 Labs',
                initial_sidebar_state = 'expanded')
+st.title ("IST 488 Labs")
+lab1 = st.Page('Labs/Lab1.py', title = 'Lab 1')
+lab2 = st.Page('Labs/Lab2.py', title = 'Lab 2', default = True)
+
+pg = st.navigation([lab2, lab1])
 pg.run()
