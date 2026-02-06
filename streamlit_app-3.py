fbe63e7f9ec23baaafdac2a24a6d2438210a052f
Monica-Lillie <152418293+mojacobs09@users.noreply.github.com>
2026-02-06 02:31:25 +0000

    updated bug

--- streamlit_app-3.py
+++ streamlit_app-3.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title = 'IST 488 Labs',
 st.title ("IST 488 Labs")
 lab1 = st.Page('Labs/Lab1.py', title = 'Lab 1')
 lab2 = st.Page('Labs/Lab2.py', title = 'Lab 2')
-lab3 = st.page('Labs/Lab3.py', title = 'Lab 3', default = True)
+lab3 = st.Page('Labs/Lab3.py', title = 'Lab 3', default = True)
 
 pg = st.navigation([lab3, lab2, lab1])
 pg.run()
