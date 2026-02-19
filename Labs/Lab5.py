f9a14008ea61e1a6ee17261047a51711bd5304fd
Monica <mojacobs@syr.edu>
2026-02-19 13:06:54 -0500

    updating code

--- Labs/Lab5.py
+++ Labs/Lab5.py
@@ -1,29 +1,46 @@
 import requests 
+import streamlit as st 
+
+
+
 # location in form City, State, Country
 # e.g. Syracuse New yORK
 # default units is in Dahrenhiet 
 
+#function to get the weather of everything
 
 def get_current_weather(location, api_key, units='imperial'):
   url = (' ')
+  response = requests.get(url)
+  if response.status_code == 401:
+    raise Exception('Authentication failed: Invalid API key (401 Unauthorized)')
+  if response.status_code == 404:
+    error_message = response.json().get('message')
+    raise Exception(f'404 error: {error_message}')
+
+  data = response.json()
+  temp = data['main']['temp']
+  feels_like = data['main']['feels_like']
+  temp_min = data['main']['temp_min']
+  temp_max = data['main']['temp_max']
+  humidity = data['main']['humidity']
+
+  return {
+    'location': location,
+    'temperature': round(temp, 2),
+    'feels_like': round(feels_like, 2),
+    'temp_min': round(temp_min, 2),
+    'temp_max': round(temp_max, 2),
+    'humidity': round(humidity, 2)
+    }
+
+weather_api_key = st.secrets['WEATHER_API_KEY']
+
+#the test of Syracuse 
+syracuse_weather = get_current_weather('Syracuse, NY, US', weather_api_key)
+print(syracuse_weather)
 
 
-response = requests.get(url)
-if response.status_code == 401:
-  raise Exception('Authentication failed: Invalid API key (401 Unauthorized)')
-if response.status_code == 404:
-  error_message = response.json().get('message')
-  raise Exception(f'404 error: {error_message}')
-data = response.json()
-temp = data['main']['temp']
-feels_like = data['main']['feels_like']
-temp_min = data['main']['temp_min']
-temp_max = data['main']['temp_max']
-humidity = data['main']['humidity']
-return {'location': location,
-  'temperature': round(temp, 2),
-  'feels_like': round(feels_like, 2),
-  'temp_min': round(temp_min, 2),
-  'temp_max': round(temp_max, 2),
-  'humidity': round(humidity, 2)
-}
+# the test of Lima, Peru
+lima_weather = get_current_weather('Lima, Peru', weather_api_key)
+print(lima_weather)
