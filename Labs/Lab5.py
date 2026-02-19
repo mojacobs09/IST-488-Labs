ef08fefa4d347187142b421b457fedabb6cb4617
Monica <mojacobs@syr.edu>
2026-02-19 13:11:48 -0500

    bug fix

--- Labs/Lab5.py
+++ Labs/Lab5.py
@@ -10,7 +10,7 @@ import streamlit as st
 #function to get the weather of everything
 
 def get_current_weather(location, api_key, units='imperial'):
-  url = (' ')
+  url = (f'https://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units={units}')
   response = requests.get(url)
   if response.status_code == 401:
     raise Exception('Authentication failed: Invalid API key (401 Unauthorized)')
