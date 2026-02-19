5d83aa3f3bc8f60eb7338b576076822e6f0fc342
Monica <mojacobs@syr.edu>
2026-02-19 14:56:33 -0500

    bug fix

--- Labs/Lab5.py
+++ Labs/Lab5.py
@@ -4,9 +4,6 @@ import json
 from openai import OpenAI
 
 
-
-
-
 # location in form City, State, Country
 # e.g. Syracuse New yORK
 # default units is in Dahrenhiet 
@@ -14,29 +11,32 @@ from openai import OpenAI
 #function to get the weather of everything
 
 def get_current_weather(location, units='imperial'):
-  url = (f'https://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units={units}')
-  response = requests.get(url)
-  if response.status_code == 401:
-    raise Exception('Authentication failed: Invalid API key (401 Unauthorized)')
-  if response.status_code == 404:
-    error_message = response.json().get('message')
-    raise Exception(f'404 error: {error_message}')
+    api_key = st.secrets["OPENWEATHERMAP_API_KEY"]
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units={units}'
 
-  data = response.json()
-  temp = data['main']['temp']
-  feels_like = data['main']['feels_like']
-  temp_min = data['main']['temp_min']
-  temp_max = data['main']['temp_max']
-  humidity = data['main']['humidity']
+    response = requests.get(url)
 
-  return {
-    'location': location,
-    'temperature': round(temp, 2),
-    'feels_like': round(feels_like, 2),
-    'temp_min': round(temp_min, 2),
-    'temp_max': round(temp_max, 2),
-    'humidity': round(humidity, 2)
-    }
+    if response.status_code == 401:
+      raise Exception('Authentication failed: Invalid API key (401 Unauthorized)')
+    if response.status_code == 404:
+      error_message = response.json().get('message')
+      raise Exception(f'404 error: {error_message}')
+
+    data = response.json()
+    temp = data['main']['temp']
+    feels_like = data['main']['feels_like']
+    temp_min = data['main']['temp_min']
+    temp_max = data['main']['temp_max']
+    humidity = data['main']['humidity']
+
+    return {
+      'location': location,
+      'temperature': round(temp, 2),
+      'feels_like': round(feels_like, 2),
+      'temp_min': round(temp_min, 2),
+      'temp_max': round(temp_max, 2),
+      'humidity': round(humidity, 2)
+      }
 
 weather_api_key = st.secrets['WEATHER_API_KEY']
 
